0adf6eb82d827df532d87e8823018042817400b1
devmdtanvir <officialtanvir@hotmail.com>
2023-02-21 10:49:51 +0600

    I am added number system for binary , octal,hexa

--- output/script.js
+++ output/script.js
@@ -16,3 +16,10 @@ const players = [];
 players.push(shakib);
 players.push(Tamim);
 console.log(players);
+// binary number before ob 
+let bin = 0b100010101010;
+let anotherBin = 0B010;
+// octal number before 0o
+let octal = 0o1012467;
+// haxadecimal before OXA
+let hexadecimal = 0XA2052ABCDEF;
--- src/script.ts
+++ src/script.ts
@@ -31,3 +31,15 @@ players.push(Tamim);
 
 console.log(players)
 
+
+
+// binary number before ob 
+let bin = 0b100010101010;
+let anotherBin: number = 0B010;
+
+// octal number before 0o
+let octal = 0o1012467;
+
+// haxadecimal before OXA
+
+let hexadecimal = 0XA2052ABCDEF;
