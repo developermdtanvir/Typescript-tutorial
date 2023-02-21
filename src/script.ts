7bfe81153211b9480b7903a1838ffca8e947a0ca
devmdtanvir <officialtanvir@hotmail.com>
2023-02-21 07:00:54 +0600

    I am added all file

---
+++ src/script.ts
@@ -0,0 +1,40 @@
+// const multiplay = (num1 : number, num2 : number) =>{
+//     return num1 * num2;
+// }
+
+// console.log(multiplay(20,10))
+
+/*
+let frutis : string[]
+
+frutis =  ['apple','banana','orange'];
+
+frutis.push('piplines');
+
+console.log(frutis);
+*/
+
+/*
+let mixed: (string | number | boolean )[]
+
+mixed = ['Tanvir',true,30];
+
+mixed.push('Shakib');
+
+console.log(mixed);
+
+*/
+
+let person : {
+    name:string,
+    captin: boolean,
+    age:number
+}
+
+person = {
+    name:"Shakib",
+
+    captin:true,
+
+    age:35
+}
